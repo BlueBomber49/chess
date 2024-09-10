e4acc39119bff1b08f0b143713548511c7bf14c3
Tony Johnson <tonyjohnson15691@gmail.com>
2024-09-09 20:12:39 -0600

    Fixed(BishopMoveCalculator) to handle pieces on board correctly.  BishopMoveTests passing

--- shared/src/main/java/chess/BishopMoveCalculator.java
+++ shared/src/main/java/chess/BishopMoveCalculator.java
@@ -18,13 +18,76 @@ public class BishopMoveCalculator extends MoveCalculator {
   public ArrayList<ChessMove> CalculateValidMoves(){
     ChessPosition start = super.position;
     ChessPosition end = start;
+    ChessGame.TeamColor color = super.color;
     ArrayList<ChessMove> moveList = new ArrayList<>();
 
-    while(end.getRow() < 8 && end.getColumn() < 8){ //Increase row and column (Upward right diagonal)
+    while(end.getRow() < 8 && end.getColumn() < 8){ //Increase row and increase column (Upward right diagonal)
       end = new ChessPosition(end.getRow()+1, end.getColumn()+1);
-      if(board.getPiece(end) == null){
+      if(board.getPiece(end) == null){  //Empty end space
         ChessMove moveToAdd = new ChessMove(start,end,null);
         moveList.add(moveToAdd);
+      }
+      else if(board.getPiece(end).getTeamColor() != color){ //Opponent's piece on end space
+        ChessMove moveToAdd = new ChessMove(start,end,null);
+        moveList.add(moveToAdd);
+        break;
+      }
+      else{ //Friendly piece on end space
+        break;
+      }
+    }
+
+    end = start; //reset end position
+
+    while(end.getRow() < 8 && end.getColumn() > 1){ //Increase row and decrease column (Upward left diagonal)
+      end = new ChessPosition(end.getRow()+1, end.getColumn()-1);
+      if(board.getPiece(end) == null){  //Empty end space
+        ChessMove moveToAdd = new ChessMove(start,end,null);
+        moveList.add(moveToAdd);
+      }
+      else if(board.getPiece(end).getTeamColor() != color){ //Opponent's piece on end space
+        ChessMove moveToAdd = new ChessMove(start,end,null);
+        moveList.add(moveToAdd);
+        break;
+      }
+      else{ //Friendly piece on end space
+        break;
+      }
+    }
+
+    end = start; //reset end position
+
+    while(end.getRow() > 1 && end.getColumn() < 8){ //Decrease row and increase column (Downward right diagonal)
+      end = new ChessPosition(end.getRow()-1, end.getColumn()+1);
+      if(board.getPiece(end) == null){  //Empty end space
+        ChessMove moveToAdd = new ChessMove(start,end,null);
+        moveList.add(moveToAdd);
+      }
+      else if(board.getPiece(end).getTeamColor() != color){ //Opponent's piece on end space
+        ChessMove moveToAdd = new ChessMove(start,end,null);
+        moveList.add(moveToAdd);
+        break;
+      }
+      else{ //Friendly piece on end space
+        break;
+      }
+    }
+
+    end = start; //reset end position
+
+    while(end.getRow() > 1 && end.getColumn() > 1){ //Decrease row and decrease column (Downward left diagonal)
+      end = new ChessPosition(end.getRow()-1, end.getColumn()-1);
+      if(board.getPiece(end) == null){  //Empty end space
+        ChessMove moveToAdd = new ChessMove(start,end,null);
+        moveList.add(moveToAdd);
+      }
+      else if(board.getPiece(end).getTeamColor() != color){ //Opponent's piece on end space
+        ChessMove moveToAdd = new ChessMove(start,end,null);
+        moveList.add(moveToAdd);
+        break;
+      }
+      else{ //Friendly piece on end space
+        break;
       }
     }
     return moveList;
