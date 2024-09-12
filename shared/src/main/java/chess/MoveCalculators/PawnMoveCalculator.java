22039993a998836cfd244aa7f356dd45ae030b04
Tony Johnson <tonyjohnson15691@gmail.com>
2024-09-11 21:10:46 -0600

    Updated(PawnMoveCalculator) with skeleton

--- shared/src/main/java/chess/MoveCalculators/PawnMoveCalculator.java
+++ shared/src/main/java/chess/MoveCalculators/PawnMoveCalculator.java
@@ -16,15 +16,47 @@ public class PawnMoveCalculator extends MoveCalculator{
   }
 
   public ArrayList<ChessMove> CalculateValidMoves(){
-    throw new RuntimeException("MoveCalculator Not Implemented");
+    ChessPosition start = super.position;
+    ChessPosition end = start;
+    ChessGame.TeamColor color = super.color;
+    ArrayList<ChessMove> moveList = new ArrayList<>();
+
+    if(color == ChessGame.TeamColor.WHITE){ //White pawn moves
+      //Edge cases
+      if(start.getRow() == 2){ //Double move option
+
+      } else if(start.getRow() == 5){ //En Passant
+
+      }
+
+
+      if(start.getColumn() != 1){ //Right capture possible
+
+      }
+      if(start.getColumn() != 8){ //Left capture possible
+
+      }
+
+      //Create all moves (move forward if nobody's in the way, capture if there are pieces to capture)
+
+      if(start.getRow() == 7){ //Promotion pieces need added to all moves  Will affect all moves
+
+      }
+
+
+    } else{ //Black pawn moves
+
+    }
     //White: moves by increasing row.
     //If row = 2, can move up 2
     //Can capture if enemy piece is diagonally forward (Col +1/-1)
-    //If row = 8, valid moves = all promotion pieces
+    //If row = 7, valid moves = all promotion pieces
 
     //Black: moves by decreasing row
     //If row = 7, can move down 2
     //Can capture if enemy piece is diagonally down (Col +1/-1)
-    //If row = 1, valid moves = all promotion pieces
+    //If row = 2, valid moves = all promotion pieces
+
+    return moveList;
   }
 }
