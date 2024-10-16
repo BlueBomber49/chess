e0c04cff4fad943d455abba43f567410954debda
Tony Johnson <tonyjohnson15691@gmail.com>
2024-10-16 08:39:23 -0600

    Updated(Server) with stubbed out users endpoint

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,20 @@
 package server;
 
+import service.*;
 import spark.*;
 
 public class Server {
+    private AdminService admin;
+    private AuthService auth;
+    private GameService game;
+    private UserService user;
+
+    public Server(){
+        this.admin = new AdminService();
+        this.auth = new AuthService();
+        this.game = new GameService();
+        this.user = new UserService();
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,11 +22,12 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-
+        Spark.get("/users", (req, res) -> "It works!");
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
         Spark.awaitInitialization();
+        System.out.printf("Listening on port %d", desiredPort);
         return Spark.port();
     }
 
