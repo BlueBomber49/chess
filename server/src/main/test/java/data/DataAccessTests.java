3a05be5f1a0ef001bf393d839d66b9a0063e4097
Tony Johnson <tonyjohnson15691@gmail.com>
2024-10-17 16:31:27 -0600

    Created(DataAccessTests) and tests are working

--- server/src/main/test/java/data/DataAccessTests.java
+++ server/src/main/test/java/data/DataAccessTests.java
@@ -1,4 +1,37 @@
-package java.data;
+import dataaccess.DataAccess;
+import dataaccess.MemoryDataAccess;
+import model.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DataAccessTests {
+
+  DataAccess data;
+  UserData bob;
+  UserData felix;
+  @BeforeEach
+  public void setup(){
+    data = new MemoryDataAccess();
+    bob = new UserData("bob", "canwefixit", "yes@wecan");
+    felix = new UserData("felix", "icanfixit", "im@gonnawreckit");
+  }
+
+  @Test
+  public void addUserTest(){
+    assertNull(data.getUser(bob.username()));
+    data.addUser(bob);
+    assertEquals(bob, data.getUser(bob.username()));
+  }
+
+  @Test
+  public void deleteUserTest(){
+    data.addUser(felix);
+    assertEquals(felix, data.getUser(felix.username()));
+    data.deleteUser("felix");
+    assertNull(data.getUser(felix.username()));
+  }
+
+
 }
