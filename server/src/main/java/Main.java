9bc136ca805dda4ca8dcac63874cbee06e37c910
Tony Johnson <tonyjohnson15691@gmail.com>
2024-10-30 17:50:58 -0600

    Refactored directory names

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -10,7 +10,7 @@ import static java.lang.Integer.parseInt;
 
 public class Main {
     public static void main(String[] args) throws ResponseException {
-        var SQL = new SQLDataAccess();
+        var Sql = new SQLDataAccess();
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
         var server = new Server();
