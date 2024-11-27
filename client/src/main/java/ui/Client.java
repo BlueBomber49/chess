86eaaeaaa10ae621d6cfe67b4d15510e48e13460
Tony Johnson <tonyjohnson15691@gmail.com>
2024-11-27 15:22:28 -0700

    Added inGameEvaluator and cleaned up client

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -2,6 +2,7 @@ package ui;
 
 import chess.*;
 import exception.ResponseException;
+import ui.evaluators.InGameEvaluator;
 import ui.evaluators.LoggedInEvaluator;
 import ui.evaluators.LoggedOutEvaluator;
 import websocket.messages.ServerMessage;
@@ -24,6 +25,7 @@ public class Client implements NotificationHandler {
   private ChessGame.TeamColor color;
   private Integer currentGameID;
   private LoggedOutEvaluator loggedOutEvaluator;
+  private ChessGame game;
   public Client(String url){
     facade = new ServerFacade(url);
     state = State.LOGGED_OUT;
@@ -76,33 +78,17 @@ public class Client implements NotificationHandler {
           return returnString;
         }
         case PLAYING_GAME, OBSERVING_GAME -> {
-          return inGameEval(cmd, params);
+          InGameEvaluator inGameEvaluator = new InGameEvaluator(auth, currentGameID, state, game, color, this, ws);
+          String message = inGameEvaluator.eval(cmd, params);
+          auth = inGameEvaluator.getAuth();
+          currentGameID = inGameEvaluator.getCurrentGameID();
+          state = inGameEvaluator.getState();
+          game = inGameEvaluator.getGame();
+          color = inGameEvaluator.getColor();
+          ws = inGameEvaluator.getWs();
+          return message;
         }
         default -> {return "Broke";}
-      }
-    }
-  }
-
-  private String inGameEval(String cmd, String[] params) {
-    switch(cmd) {
-      case "leave" -> {
-        return leaveGame();
-      }
-      case "redraw" -> {
-        return "Chessboard goes here";
-        //drawBoard();
-      }
-      case "resign" -> {
-        return resignGame();
-      }
-      case "highlight" -> {
-        return "I would love to highlight the board right now";
-      }
-      case "move" -> {
-        return makeMove(params);
-      }
-      default -> {
-        return "Not a recognized command.  Type 'help' for a list of commands";
       }
     }
   }
@@ -139,77 +125,6 @@ public class Client implements NotificationHandler {
               """;
       default -> "Something broke";
     };
-  }
-
-  public String leaveGame(){
-    try {
-      state=State.LOGGED_IN;
-      ws.leaveGame(auth, currentGameID);
-      return "You have left the game";
-    }
-    catch (Exception e){
-      return "Error: " + e.getMessage();
-    }
-  }
-
-  public String resignGame(){
-    try{
-      ws.resignGame(auth, currentGameID);
-    } catch (ResponseException e) {
-      return "Error: " + e.getMessage();
-    }
-    return "You have resigned";
-  }
-
-  public String makeMove(String[] params){
-    try {
-      if (params.length < 2 || params.length > 3) {
-        return "Please use format 'move [start column][start row] [end column][end row] [Promotion R|B|N|Q]";
-      }
-      var colMap=Map.of("a", 1, "b", 2, "c", 3, "d", 4,
-              "e", 5, "f", 6, "g", 7, "h", 8);
-      String start=params[0];
-      String end=params[1];
-      if (!start.matches("\\b[a-h][1-8]\\b") || !end.matches("\\b[a-h][1-8]\\b")) {
-        return "Please use format 'move [start column][start row] [end column][end row]";
-      }
-      int startRow=Integer.parseInt(start.substring(1));
-      int startCol=colMap.get(start.substring(0, 1));
-      int endRow=Integer.parseInt(end.substring(1));
-      int endCol=colMap.get(end.substring(0, 1));
-      ChessPosition startPos=new ChessPosition(startRow, startCol);
-      ChessPosition endPos=new ChessPosition(endRow, endCol);
-      ChessMove move;
-      if (params.length == 3) {
-        String promotionParam=params[2];
-        ChessPiece.PieceType promotionPiece;
-        switch (promotionParam.toUpperCase()) {
-          case "R" -> {
-            promotionPiece=ChessPiece.PieceType.ROOK;
-          }
-          case "N" -> {
-            promotionPiece=ChessPiece.PieceType.KNIGHT;
-          }
-          case "B" -> {
-            promotionPiece=ChessPiece.PieceType.BISHOP;
-          }
-          case "Q" -> {
-            promotionPiece=ChessPiece.PieceType.QUEEN;
-          }
-          default -> {
-            return "Invalid Promotion Piece";
-          }
-        }
-        move=new ChessMove(startPos, endPos, promotionPiece);
-      } else {
-        move=new ChessMove(startPos, endPos, null);
-      }
-      ws.makeMove(auth, currentGameID, move);
-    }
-    catch(Exception e){
-      return "Error: " + e.getMessage();
-    }
-    return "";
   }
 
   public String drawBoard(ChessGame game, ChessGame.TeamColor perspective){
@@ -299,6 +214,7 @@ public class Client implements NotificationHandler {
     switch(type) {
       case LOAD_GAME -> {
         ChessGame game = message.getGame();
+        this.game = game;
         drawBoard(game, color);
       }
       case NOTIFICATION -> {
@@ -308,6 +224,5 @@ public class Client implements NotificationHandler {
         System.out.println("Error: " + message.getErrorMessage());
       }
     }
-
   }
 }
--- client/src/main/java/ui/evaluators/InGameEvaluator.java
+++ client/src/main/java/ui/evaluators/InGameEvaluator.java
@@ -1,4 +1,149 @@
 package ui.evaluators;
 
+import chess.*;
+import exception.ResponseException;
+import ui.Client;
+import ui.State;
+import websocket.WebsocketFacade;
+
+import java.util.Locale;
+import java.util.Map;
+
+import static ui.EscapeSequences.*;
+
 public class InGameEvaluator {
+
+  ChessGame game;
+  ChessGame.TeamColor color;
+  Client client;
+  WebsocketFacade ws;
+  String auth;
+  Integer currentGameID;
+  State state;
+  public InGameEvaluator(String auth, Integer currentGameID, State state, ChessGame game, ChessGame.TeamColor color, Client client, WebsocketFacade ws){
+    this.game = game;
+    this.color = color;
+    this.client = client;
+    this.ws = ws;
+    this.auth = auth;
+    this.currentGameID = currentGameID;
+    this.state = state;
+  }
+
+  public String eval(String cmd, String[] params) {
+    switch(cmd) {
+      case "leave" -> {
+        return leaveGame();
+      }
+      case "redraw" -> {
+        return client.drawBoard(game, color);
+      }
+      case "resign" -> {
+        return resignGame();
+      }
+      case "highlight" -> {
+        return "I would love to highlight the board right now";
+      }
+      case "move" -> {
+        return makeMove(params);
+      }
+      default -> {
+        return "Not a recognized command.  Type 'help' for a list of commands";
+      }
+    }
+  }
+  public String leaveGame(){
+    try {
+      state=State.LOGGED_IN;
+      ws.leaveGame(auth, currentGameID);
+      return "You have left the game";
+    }
+    catch (Exception e){
+      return "Error: " + e.getMessage();
+    }
+  }
+
+  public String resignGame(){
+    try{
+      ws.resignGame(auth, currentGameID);
+    } catch (ResponseException e) {
+      return "Error: " + e.getMessage();
+    }
+    return "You have resigned";
+  }
+
+  public String makeMove(String[] params){
+    try {
+      if (params.length < 2 || params.length > 3) {
+        return "Please use format 'move [start column][start row] [end column][end row] [Promotion R|B|N|Q]";
+      }
+      var colMap=Map.of("a", 1, "b", 2, "c", 3, "d", 4,
+              "e", 5, "f", 6, "g", 7, "h", 8);
+      String start=params[0];
+      String end=params[1];
+      if (!start.matches("\\b[a-h][1-8]\\b") || !end.matches("\\b[a-h][1-8]\\b")) {
+        return "Please use format 'move [start column][start row] [end column][end row]";
+      }
+      int startRow=Integer.parseInt(start.substring(1));
+      int startCol=colMap.get(start.substring(0, 1));
+      int endRow=Integer.parseInt(end.substring(1));
+      int endCol=colMap.get(end.substring(0, 1));
+      ChessPosition startPos=new ChessPosition(startRow, startCol);
+      ChessPosition endPos=new ChessPosition(endRow, endCol);
+      ChessMove move;
+      if (params.length == 3) {
+        String promotionParam=params[2];
+        ChessPiece.PieceType promotionPiece;
+        switch (promotionParam.toUpperCase()) {
+          case "R" -> {
+            promotionPiece=ChessPiece.PieceType.ROOK;
+          }
+          case "N" -> {
+            promotionPiece=ChessPiece.PieceType.KNIGHT;
+          }
+          case "B" -> {
+            promotionPiece=ChessPiece.PieceType.BISHOP;
+          }
+          case "Q" -> {
+            promotionPiece=ChessPiece.PieceType.QUEEN;
+          }
+          default -> {
+            return "Invalid Promotion Piece";
+          }
+        }
+        move=new ChessMove(startPos, endPos, promotionPiece);
+      } else {
+        move=new ChessMove(startPos, endPos, null);
+      }
+      ws.makeMove(auth, currentGameID, move);
+    }
+    catch(Exception e){
+      return "Error: " + e.getMessage();
+    }
+    return "";
+  }
+
+  public String getAuth() {
+    return auth;
+  }
+
+  public Integer getCurrentGameID() {
+    return currentGameID;
+  }
+
+  public State getState() {
+    return state;
+  }
+
+  public ChessGame getGame() {
+    return game;
+  }
+
+  public ChessGame.TeamColor getColor() {
+    return color;
+  }
+
+  public WebsocketFacade getWs() {
+    return ws;
+  }
 }
